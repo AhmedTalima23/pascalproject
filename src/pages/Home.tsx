37ed8c09eadb2d6f1a0537406614005d49c7313a
Ahmed Talima <162712731+AhmedTalima23@users.noreply.github.com>
2025-09-02 23:15:36 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -171,7 +171,7 @@ const Home = () => {
                 }}
                 whileTap={{ scale: 0.95 }}
               >
-                View Our Eventssss
+                View Our Events
                 <ArrowRight className="ml-2 h-5 w-5 group-hover:translate-x-1 transition-transform" />
               </motion.a>
               <motion.a
