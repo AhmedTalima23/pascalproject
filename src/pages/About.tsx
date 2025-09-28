85eb9a3978fc291cfd5ace50bf9b9d5eb25f1ab8
Ahmed Talima <162712731+AhmedTalima23@users.noreply.github.com>
2025-09-28 13:16:54 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -72,7 +72,7 @@ const About = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, ease: 'easeOut' }}
           >
-            About Pascalllll
+            About Pascal
           </motion.h1>
           <motion.p
             className="text-xl md:text-2xl text-blue-100 max-w-3xl mx-auto"
